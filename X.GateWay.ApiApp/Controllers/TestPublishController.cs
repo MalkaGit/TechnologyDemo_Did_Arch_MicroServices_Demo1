cfd7b02d77b392bbba9e8b65f65b50b6a5eb1967
MalkaGit <regine.issan@gmail.com>
2020-08-20 16:16:57 +0300

    m

---
+++ X.GateWay.ApiApp/Controllers/TestPublishController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace X.GateWay.ApiApp.Controllers
+{
+    [Route("")]
+    public class TestPublishController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Content("Hello from Test Publish Controller API!");
+        }
+    }
+
+
+
+
+}
